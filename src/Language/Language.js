d6a720fd878fa5c65b9ac48b825269b170bdac8c
AlexxMaker <Alexeus92@mail.ru>
2019-02-17 22:38:45 +0430

    Ruflag FIXED! Hallelujah!

--- src/Language/Language.js
+++ src/Language/Language.js
@@ -9,15 +9,13 @@ class Language extends Component {
         return (
             <div className="langbar">
                 <div className="lang_pill">
-                    <img src={flag_us} alt="EN" className="lang_img"></img>
+                    <img src={flag_us} alt="EN"></img>
                     <p>ENG</p>
                 </div>
 
                 <div className="lang_pill">
-                    <div>
-                        <img src={flag_rus} alt="RU" className="lang_img"></img>
-                        <p>RUS</p>
-                    </div>
+                    <img src={flag_rus} alt="RU"></img>
+                    <p>RUS</p>
                 </div>
             </div>
         );
